2f6ca7b9a4253b2e3dc9fd034a252e2ac96678ae
Francisco Paz <fjpazf@unah.hn>
2025-04-22 01:35:00 -0400

    primera version .1

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://franciscopaz199.github.io/nexoSolutions',
-  base: '/nexoSolutions/', // muy importante este slash
+  site: 'https://franciscopaz199.github.io',
+  base: '/nexoSolutions', // muy importante este slash
   outDir: './dist', // carpeta de salida
   build: {
     format: 'directory', // más compatible para GH Pages
